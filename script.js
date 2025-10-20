e52fbf4bc28eae3f20847caef4b0fcd771b6c590
Swaroop P <swaroop07p@gmail.com>
2025-10-20 21:36:20 +0530

    Initial Intermediate Commit

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("Amazon");
+
+let right_btn=document.getElementById("right-button");
+let left_btn=document.getElementById("left-button");
+let home_img=document.getElementById("home-image");
+
+let img_arr=["Images/HomePage1.jpg","Images/HomePage2.jpg","Images/HomePage3.jpg","Images/HomePage.jpg"];
+
+let i=0;
+
+right_btn.addEventListener("click",()=>{
+    home_img.style.backgroundImage=`url(${img_arr[i]})`;
+    i+=1;
+    if (i>3){
+        i=0;
+    }
+});
+
+let j=2;
+left_btn.addEventListener("click",()=>{
+    home_img.style.backgroundImage=`url(${img_arr[j]})`;
+    j--;
+    if (j<0){
+        j=3
+    }
+});
