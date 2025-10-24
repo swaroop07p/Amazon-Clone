d172ac4aeb114ffa76b7f04dfa73b84cfb6dc15d
Swaroop P <swaroop07p@gmail.com>
2025-10-24 22:45:26 +0530

    Script

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ let right_btn=document.getElementById("right-button");
 let left_btn=document.getElementById("left-button");
 let home_img=document.getElementById("home-image");
 
+let rect1=document.getElementById("rect1");
+let rect2=document.getElementById("rect2");
+let rect3=document.getElementById("rect3");
+let rect_left_btn=document.getElementById("left-rect-btn");
+let rect_right_btn=document.getElementById("right-rect-btn");
+
 let img_arr=["Images/HomePage1.jpg","Images/HomePage2.jpg","Images/HomePage3.jpg","Images/HomePage.jpg"];
 
 let i=0;
@@ -24,3 +30,18 @@ left_btn.addEventListener("click",()=>{
         j=3
     }
 });
+
+rect_right_btn.addEventListener("click",()=>{
+    let r1=Math.floor(Math.random()*3);
+    let r2=Math.floor(Math.random()*3);
+    let r3=Math.floor(Math.random()*3);
+    console.log(r1);
+    console.log(r2);
+    console.log(r3);
+    if (r1!=r2 && r2!=r3 && r1!=r3){
+        rect1.style.zIndex=r1;
+        rect2.style.zIndex=r2;
+        rect3.style.zIndex=r3;
+    }
+    
+})
